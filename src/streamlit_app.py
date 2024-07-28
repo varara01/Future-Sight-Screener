b56a0972f3146268f9aa5e1791bd1998b9f06f9d
Dhruv Malani <118156335+Malanidhruv@users.noreply.github.com>
2024-07-29 01:48:37 +0530

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -268,9 +268,9 @@ def get_extra_inputs(tickerOption, executeOption, c_index=None, c_criteria=None,
 
 ac, bc = st.columns([13,1])
 
-ac.title('📈 Screeni-py')
+ac.title('📈 Future Sight')
 if guiUpdateMessage == "":
-  ac.subheader('Find Breakouts, Just in Time!')
+  ac.subheader('Your pathway to success')
 
 if isDevVersion:
     ac.warning(guiUpdateMessage, icon='⚠️')
